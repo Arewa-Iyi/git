902bf309f10843f35e50cbb20be020daf180e47f
Arewa Iyi <arewaiyi@gmail.com>
2024-02-14 10:40:25 -0500

    updated brackets for charArray

--- CountLettersInArray.java
+++ CountLettersInArray.java
@@ -6,7 +6,7 @@ This code if for furture refrence and will not run
 class RandomCharacter {
   /** Generate a random character between ch1 and ch2 */
   public static char getRandomCharacter(char ch1, char ch2) {
-    return (char)(ch1 + Math.random() * (ch2 - ch1 + 1));
+    return ((char)(ch1 + Math.random() * (ch2 - ch1 + 1)));
   }
 
   /** Generate a random lowercase letter */
